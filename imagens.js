f87f641346ac476218241be1dc3c20d381beaf2b
caiopatriota <98909537+Legalso@users.noreply.github.com>
2023-01-18 09:59:48 -0300

    v2.0.3
    Teste do gif e da imagem do ator.

--- imagens.js
+++ imagens.js
@@ -14,8 +14,10 @@ let somDoPonto;
 function preload(){
   //imagemDaEstrada = loadImage("imagens/estrada.png");
   imagemDaEstrada = loadImage("imagens/space.png");
-  imagemDoAtor = loadImage("imagens/ator-1.png");
-  imagemCarro = loadImage("imagens/carro-1.png");
+  //imagemDoAtor = loadImage("imagens/ator-1.png");
+  imagemDoAtor = loadImage("imagens/spaceship.png");
+  //imagemCarro = loadImage("imagens/carro-1.png");
+  imagemCarro = loadImage("imagens/ufo.gif");
   imagemCarro2 = loadImage("imagens/carro-2.png");
   imagemCarro3 = loadImage("imagens/carro-3.png");
   imagemCarros = [imagemCarro, imagemCarro2, imagemCarro3, imagemCarro, imagemCarro2, imagemCarro3]
