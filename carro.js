5d35d1c9f70e88b62b72a7f8bd16d9c2b13d5838
caiopatriota <98909537+Legalso@users.noreply.github.com>
2023-01-18 12:49:58 -0300

    v2.1.5

--- carro.js
+++ carro.js
@@ -3,8 +3,8 @@
 let xCarros = [600, 600, 600,600, 600,600];
 let yCarros = [40,96,150, 210, 270, 318];
 let velocidadeCarros = [2, 2.5, 3.2, 5, 3.3, 2.3];
-let comprimentoCarro = 50;
-let alturaCarro = 40;
+let comprimentoCarro = 45;
+let alturaCarro = 35;
 let n = xCarros.length;
 
 
